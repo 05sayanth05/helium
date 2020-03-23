1b9c93a32f405f7e7bbd7218cd339ea4f7593d3c
sayanth <sayanths05@gmail.com>
2020-03-23 23:48:46 +0530

    file upload using express-fileupload done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # helium
+
+# changes need to done:
+    * remove express-fileupload (too slow on large size files) and it uploads files to RAM and then moves to secondary storage  
+    * add formidable instead of express-file upload (check speed)
---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+
+const app = express();
+
+const uploadPath = "E:\\helium\\";
+
+app.set("view engine","ejs");
+app.use(fileUpload({
+    debug : true
+}));
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+
+app.get("/upload",(req,res) => {
+    res.render("uploadform");
+});
+
+app.post("/upload",(req,res) => {
+    let file = req.files.f;
+
+    file.mv(uploadPath+file.name, (err) => {
+        if(err) {
+            console.log(err);
+            return res.status(500).send("failed to move");
+        }
+        res.redirect("/upload");
+    });
+
+});
+
+app.listen(3000,() => {
+    console.log("server started at port 3000");
+
+    if(!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath);
+        console.log("directory created | path: "+uploadPath);
+    }
+
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+      
+        ifaces[ifname].forEach(function (iface) {
+          if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+          }
+      
+          if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+          } else {
+            // this interface has only one ipv4 adress
+            console.log("\x1b[32m%s\x1b[0m","ip address: "+ifname+": "+iface.address);
+          }
+          ++alias;
+        });
+      });
+
+});
